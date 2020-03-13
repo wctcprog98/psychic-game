d09f3de16c559a90ae809b64cfbc5be1596cd551
root <chinenyediei@gmail.com>
2020-03-12 23:54:54 -0500

    win and lose functions working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,60 @@
 //global variables
-
 var userGuess = "";
 var wordsArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var secretLetter = "";
-var guesses = 0;
+var letter = ""; //used to clear data 
 
 //start game
 $(document).ready(function () {
 
+
+   
+    {
+        
+       
     function startGame() {
         //hide winning div
         $(".winDiv").hide();
-
         //hide game over div
-        $(".gameOver").hide(); 
-        
+        $(".gameOver").hide();
         //set/reset variables
         var guessesRemaining = 10;
         $("#guessesRemaining").text(guessesRemaining);
         $("#letterGuessed").text("");
+        $(".highLow").text("");
 
         //gets randomword from array
         secretLetter = wordsArray[Math.floor(Math.random() * wordsArray.length)];
         console.log("Secret Letter is " + secretLetter);
 
         //capture user input
-        document.onkeypress = function (event) {
-            //capture input and convert captial letters to lowercase
-            userGuess = event.key.toLowerCase();
-            //reduce guesses remaining
-            guessesRemaining--;
+            document.onkeypress = function (event) {
+                //capture input and convert captial letters to lowercase
+                userGuess = event.key.toLowerCase();
+                //reduce guesses remaining
+                guessesRemaining--;
+                //append remaining guesses to screen
+                $("#guessesRemaining").text(guessesRemaining);
 
-            //append remaining guesses to screen
-            $("#guessesRemaining").text(guessesRemaining);
+                //append userGuess to screen
+                $("#letterGuessed").text(userGuess);
 
-            //append userGuess to screen
-            $("#letterGuessed").text(userGuess);
+                //test to see if capturing correct letter
+                console.log(userGuess);
 
-            //test to see if capturing correct letter
-            console.log(userGuess);
+                //run comparison to see if letter guessed is higher or lower than the secret letter  
+                highLow();
+                if (guessesRemaining === 0) {
+                    $(document).off('onkeypress');
+                    console.log("hi"); 
+                    //show game over div
+                    $(".gameOver").show();
+                    $("#playAgain").on("click", function () {
+                        startGame();
+                    });
+                }
 
-            //run comparison to see if letter guessed is higher or lower than the secret letter
-            highLow();
+            }
         }
 
     }
@@ -50,7 +63,7 @@ $(document).ready(function () {
     function highLow() {
         if (userGuess === secretLetter) {
             $(".winDiv").show();
-               //restart game
+            //restart game
             $("#play").on("click", function () {
                 startGame();
             });
@@ -60,23 +73,9 @@ $(document).ready(function () {
         }
         else
             $(".highLow").text("LOWER");
-    
-    //
-        function gameOver() {
-            //check if value is equal to zero
-            if (guessesRemaining === 0)
-            {
-                //show game over div
-                $()
-                }
-        }
-        
     }
+
     startGame();
-
-    
-
-
 
 });
 
