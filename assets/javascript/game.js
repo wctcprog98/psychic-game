6ce200cdfbbe13e1b802361f3d9ba051b8889957
root <chinenyediei@gmail.com>
2020-03-12 19:29:21 -0500

    program working, adding validation to ensure numbers don't go negative

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,90 +1,82 @@
 //global variables
 
 var userGuess = "";
-var wins = 0;
-var losses = 0;
-var lettersGuessed = [];
 var wordsArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var secretLetter = "";
 var guesses = 0;
-var guessesRemaining = 10;
 
-//create startup function
-//Hide gameOver
-function startGame() {
-     //gets randomword from array
-     secretLetter = wordsArray[Math.floor(Math.random() * wordsArray.length)];
-     console.log("Secret Letter is " + secretLetter);
-    console.log(userGuess);
+//start game
+$(document).ready(function () {
 
-    $(".gameOver").hide();
-    $(".gameDiv").show();
-    $("#guessesRemaining").text()
+    function startGame() {
+        //hide winning div
+        $(".winDiv").hide();
 
-    //update values  
-    lettersGuessed = ""; 
-    guessesRemaining = 10; 
-    //change HTML 
-    $("#guessesRemaining").text(guessesRemaining);
-    $("#letterGuessed").text(lettersGuessed); 
+        //hide game over div
+        $(".gameOver").hide(); 
+        
+        //set/reset variables
+        var guessesRemaining = 10;
+        $("#guessesRemaining").text(guessesRemaining);
+        $("#letterGuessed").text("");
 
-}
-function highLow() {
-    if (userGuess === secretLetter) {
-        $(".highLow").text("WINNER"); 
+        //gets randomword from array
+        secretLetter = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+        console.log("Secret Letter is " + secretLetter);
+
+        //capture user input
+        document.onkeypress = function (event) {
+            //capture input and convert captial letters to lowercase
+            userGuess = event.key.toLowerCase();
+            //reduce guesses remaining
+            guessesRemaining--;
+
+            //append remaining guesses to screen
+            $("#guessesRemaining").text(guessesRemaining);
+
+            //append userGuess to screen
+            $("#letterGuessed").text(userGuess);
+
+            //test to see if capturing correct letter
+            console.log(userGuess);
+
+            //run comparison to see if letter guessed is higher or lower than the secret letter
+            highLow();
+        }
+
     }
-    else if (userGuess < secretLetter) {
-        $(".highLow").text("HIGHER"); 
-    }
-    else
-    $(".highLow").text("LOWER"); 
-}
-startGame();
 
-//function to hide div
-function gameOver() {
-    if (guessesRemaining === 0) {
-
-        $(".gameDiv").hide();
-        $(".gameOver").show();
-        $("#playAgain").on("click", function () {
-            startGame(); 
-        })
+    //check if value is higher or lower than letter guessed
+    function highLow() {
+        if (userGuess === secretLetter) {
+            $(".winDiv").show();
+               //restart game
+            $("#play").on("click", function () {
+                startGame();
+            });
+        }
+        else if (userGuess < secretLetter) {
+            $(".highLow").text("HIGHER");
+        }
+        else
+            $(".highLow").text("LOWER");
+    
+    //
+        function gameOver() {
+            //check if value is equal to zero
+            if (guessesRemaining === 0)
+            {
+                //show game over div
+                $()
+                }
+        }
         
     }
-    else if (userGuess === secretLetter)
-    {
-        $("gameDiv").hide();
-        $(".winDiv").show();
-        }
-}
-    //function to show div
+    startGame();
 
-    //grab user keys
-    document.onkeyup = function (event) {
-        guessesRemaining--; 
-        //gets key pushed from user
-        userGuess = event.key;
-        console.log(userGuess);
-        $("#letterGuessed").text(userGuess); 
-
-        highLow();
-        //guesses text updated to guesses remaining
-        $("#guessesRemaining").text(guessesRemaining);
-        console.log(guessesRemaining);
-        if (guessesRemaining === 0) {
-            gameOver();
-        }
-
-        secretLetter = secretLetter.toLowerCase();
-        if (userGuess === secretLetter) {
-            wins++;
-            $("#wins").text(wins);
-        }
-        else {
-            losses++;
-            $("#losses").text(losses);
-        }
     
-    }
+
+
+
+});
 
